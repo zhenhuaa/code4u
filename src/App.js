5e29f2293c23ded483366ad849c4fc76d294ce4f
hzwangzhenhua <hzwangzhenhua@corp.netease.com>
2021-04-22 15:59:32 +0800

    feat: add two editor panel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Controlled as CodeMirror } from "react-codemirror2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="playground-container">
+      <div className="editors-container">
+        <div className="editors">
+          <div className="editor">
+            <CodeMirror
+              value="sample code"
+              options={{
+                mode: "javascript",
+                lineNumbers: true,
+                theme: "xq-light",
+              }}
+            />
+          </div>
+          <div className="editor">
+            <CodeMirror
+              value="sample code2 test"
+              options={{
+                mode: "javascript",
+                lineNumbers: true,
+                theme: "xq-light",
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
