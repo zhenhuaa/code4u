0cda17443245cccb84aed0d8458dc902386425b7
wangzhenhuaa <wangzhenhuaa@gmail.com>
2021-04-25 20:25:18 +0800

    feat: 🎸 add path params support

--- src/lib/openapi-typescript/transform/apiStub.ts
+++ src/lib/openapi-typescript/transform/apiStub.ts
@@ -1,11 +1,13 @@
 import _ from "lodash";
 import {
+  HttpMethod,
   OpenAPI3,
   OperationObject,
   ParameterObject,
   PathItemObject,
   ReferenceObject,
   RequestBody,
+  SchemaObject,
 } from "openapi-typescript";
 import { httpMethods } from "../../constants";
 import { getRefObject, tsUnionOf } from "../utils";
@@ -25,8 +27,9 @@ function getBaseUrl(servers: OpenAPI3["servers"]) {
   return "";
 }
 
-export function getQsList(
+export function getTypeParamsList(
   params: (ReferenceObject | ParameterObject)[] | undefined,
+  type: "path" | "query",
   schema: OpenAPI3
 ): PathItemObject[] {
   if (!params) return [];
@@ -37,11 +40,25 @@ export function getQsList(
       const refP = getRefObject<ParameterObject>(p.$ref, schema.components);
       item = refP;
     }
-    if (item.in === "query") {
+    if (item.in === type) {
       list.push(item);
     }
   }
   return list;
+}
+
+export function getPathParamsList(
+  params: (ReferenceObject | ParameterObject)[] | undefined,
+  schema: OpenAPI3
+): PathItemObject[] {
+  return getTypeParamsList(params, "path", schema);
+}
+
+export function getQsList(
+  params: (ReferenceObject | ParameterObject)[] | undefined,
+  schema: OpenAPI3
+): PathItemObject[] {
+  return getTypeParamsList(params, "query", schema);
 }
 
 export function getReqBody(reqBody: ReferenceObject | RequestBody | undefined, schema: OpenAPI3): RequestBody | null {
@@ -57,20 +74,29 @@ type TagOpMap = Record<string, OperationObject[]>;
 
 function getReqUnionTypes(op: OperationObject, schema: OpenAPI3): string[] {
   const fn = op.operationId as string;
-  const qsList = getQsList(op.parameters, schema);
 
   const unionTypes: string[] = [];
 
+  const qsList = getQsList(op.parameters, schema);
   if (!_.isEmpty(qsList)) {
     const qsType = `operations["${fn}"]["parameters"]["query"]`;
     unionTypes.push(qsType);
   }
-  const reqBody = getReqBody(op.requestBody, schema);
 
+  const pathList = getPathParamsList(op.parameters, schema);
+  if (!_.isEmpty(pathList)) {
+    const pathType = `operations["${fn}"]["parameters"]["path"]`;
+    unionTypes.push(pathType);
+  }
+
+
+  const reqBody = getReqBody(op.requestBody, schema);
   if (reqBody) {
     const bodyType = `operations["${fn}"]["requestBody"]["content"]["application/json"]`;
     unionTypes.push(bodyType);
   }
+
+
   return unionTypes;
 }
 
@@ -110,9 +136,15 @@ function isHas200JsonResponse(op: OperationObject, schema: OpenAPI3): boolean {
   if ("$ref" in response) {
     response = getRefObject(response.$ref, schema.components);
   }
-  const deepGetKey = "content.application/json.schema.properties.data";
-  const refObj = _.get(response, deepGetKey);
-  return !!refObj;
+  const deepGetKey = "content.application/json.schema";
+  const refObj: SchemaObject | ReferenceObject = _.get(response, deepGetKey);
+
+  let schemaObj = refObj || {};
+  if ("$ref" in schemaObj) {
+    const resSchema: SchemaObject = getRefObject(schemaObj.$ref, schema.components);
+    schemaObj = resSchema;
+  }
+  return !!schemaObj.properties;
 }
 
 function transResNs(tagNsMap: TagOpMap, schema: OpenAPI3) {
@@ -124,7 +156,7 @@ function transResNs(tagNsMap: TagOpMap, schema: OpenAPI3) {
       const refType = upperCamelCase(fn);
       const has200Json = isHas200JsonResponse(op, schema);
       if (has200Json) {
-        const code = `export type ${refType} = operations["${fn}"]["responses"]["200"]["content"]["application/json"]["data"]`;
+        const code = `export type ${refType} = operations["${fn}"]["responses"]["200"]["content"]["application/json"]`;
         nsLines.push(code);
       }
     }
@@ -133,6 +165,51 @@ function transResNs(tagNsMap: TagOpMap, schema: OpenAPI3) {
   }
   const reqNsCode = lines.join("\n");
   return reqNsCode;
+}
+
+function genFunCode(tag: string, url: string, m: HttpMethod, operation: OperationObject, schema: OpenAPI3) {
+  const fn = operation.operationId;
+  if (!fn) return "";
+  const reqNs = `${tag}Req`;
+  const resNs = `${tag}Res`;
+  const refType = upperCamelCase(fn);
+  let parameters = "";
+
+  const unionTypes = getReqUnionTypes(operation, schema);
+  if (!_.isEmpty(unionTypes)) {
+    parameters = `params: ${reqNs}.${refType}`;
+  }
+
+  let retType = "unknown";
+  const has200Json = isHas200JsonResponse(operation, schema);
+  if (has200Json) {
+    retType = `${resNs}.${refType}`;
+  }
+  const funcLines: string[] = [];
+  const fnStart = `export async function ${fn}(${parameters}): Promise<${retType}> {`;
+  const fnEnd = "}\n";
+
+  const pathParams = getPathParamsList(operation.parameters, schema)
+  if(pathParams.length > 0) {
+    url = url.replace(/{([a-zA-Z_]+)}/g, '${params.$1}')
+  }
+
+  funcLines.push(fnStart);
+  const paramsKey = m === "get" ? "params" : "data";
+  funcLines.push(`const data = await request({`);
+  funcLines.push(`method: "${m}",`);
+  if(pathParams.length > 0) {
+    funcLines.push(`url: \`${url}\`,`);
+  } else {
+    funcLines.push(`url: "${url}",`);
+  }
+  funcLines.push(`${paramsKey}: params`);
+  funcLines.push("})");
+  funcLines.push(`const result: ${retType} = data.data`);
+  funcLines.push(`return result`);
+  funcLines.push(fnEnd);
+  const funcCode = funcLines.join("\n");
+  return funcCode;
 }
 
 export function transApiStub(schema: OpenAPI3): string {
@@ -145,10 +222,10 @@ export function transApiStub(schema: OpenAPI3): string {
   const baseUrl = getBaseUrl(schema.servers);
   axiosLines.push('import axios from "axios"\n');
   axiosLines.push("const request = axios.create({");
-  axiosLines.push(`baseUrl: "${baseUrl}"`);
+  axiosLines.push(`baseURL: "${baseUrl}"`);
   axiosLines.push("})\n\n");
   const axiosCode = axiosLines.join("\n");
-  let shouldWriteAxiosCode = false
+  let shouldWriteAxiosCode = false;
 
   Object.entries(paths).forEach(([url, pathItem]) => {
     for (let m of httpMethods) {
@@ -159,39 +236,9 @@ export function transApiStub(schema: OpenAPI3): string {
         tag = operation.tags[0];
       }
       tag = upperCamelCase(tag);
-      const reqNs = `${tag}Req`;
-      const resNs = `${tag}Res`;
-      const fn = operation.operationId;
-      if (!fn) continue;
 
-      const refType = upperCamelCase(fn);
-      let parameters = "";
-
-      const unionTypes = getReqUnionTypes(operation, schema);
-      if (!_.isEmpty(unionTypes)) {
-        parameters = `params: ${reqNs}.${refType}`;
-      }
-
-      let retType = "unknown";
-      const has200Json = isHas200JsonResponse(operation, schema);
-      if (has200Json) {
-        retType = `${resNs}.${refType}`;
-      }
-
-      // apiStubLines.push(axiosCode)
-
-      shouldWriteAxiosCode = true
-      const funcLines: string[] = [];
-      const fnStart = `export async function ${fn}(${parameters}): Promise<${retType}> {`;
-      const fnEnd = "}\n";
-
-      funcLines.push(fnStart);
-      //TODO add function implementation
-
-      funcLines.push(fnEnd);
-
-      const funcCode = funcLines.join("\n");
-
+      shouldWriteAxiosCode = true;
+      const funcCode = genFunCode(tag, url, m, operation, schema);
       apiStubLines.push(funcCode);
 
       if (tagNsMap[tag]) {
@@ -210,10 +257,9 @@ export function transApiStub(schema: OpenAPI3): string {
   const resNsCode = transResNs(tagNsMap, schema);
   output += resNsCode;
 
-
   // gen axios code
-  if(shouldWriteAxiosCode) {
-    output += axiosCode
+  if (shouldWriteAxiosCode) {
+    output += axiosCode;
   }
 
   // generate api stub code
