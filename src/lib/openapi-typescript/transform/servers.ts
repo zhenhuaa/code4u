d541ef0a1fa84f90ee56808f67074cf7a62a6058
wangzhenhuaa <wangzhenhuaa@gmail.com>
2021-04-27 19:09:16 +0800

    feat: 🎸 add server enum trans support

--- src/lib/openapi-typescript/transform/servers.ts
+++ src/lib/openapi-typescript/transform/servers.ts
@@ -1,10 +1,10 @@
 import { OpenAPI3 } from "../types";
 import { comment, tsUnionOf } from "../utils";
 
-export function transServers(schema: OpenAPI3) {
+export function transServersToType(schema: OpenAPI3) {
   if (!schema.servers) return "";
   const servers = schema.servers;
-  const codeLines: string[] = []
+  const codeLines: string[] = [];
   let i = 0;
   const serverNames: string[] = [];
   for (let s of servers) {
@@ -15,14 +15,47 @@ export function transServers(schema: OpenAPI3) {
     const serverName = `Server${i}`;
     serverNames.push(serverName);
     const line = `${serverComment}type ${serverName} = "${s.url}";`;
-    codeLines.push(line)
+    codeLines.push(line);
     i += 1;
   }
 
   if (serverNames.length > 0) {
     const line = `export type Servers = ${tsUnionOf(serverNames)};\n\n`;
-    codeLines.push(line)
+    codeLines.push(line);
   }
-  const code = codeLines.join("\n")
+  const code = codeLines.join("\n");
   return code;
+}
+
+export function transServers(schema: OpenAPI3) {
+  const enumCode = transServersToEnum(schema);
+  return enumCode;
+}
+
+export function transServersToEnum(schema: OpenAPI3) {
+  if (!schema.servers) return "";
+  const servers = schema.servers;
+  const enumLines: string[] = [];
+  let i = 0;
+  const serverNames: string[] = [];
+  for (let s of servers) {
+    let serverComment = "";
+    if (s.description) {
+      serverComment = comment(s.description);
+    }
+    const serverName = `Server${i}`;
+    serverNames.push(serverName);
+    const line = `${serverComment}${serverName} = "${s.url}",`;
+    enumLines.push(line);
+    i += 1;
+  }
+
+  if (serverNames.length > 0) {
+    const fstLine = "export enum EServers {";
+    const endLine = "}\n\n";
+    const codeLines = [fstLine, ...enumLines, endLine];
+    const code = codeLines.join("\n");
+    return code;
+  }
+  return "";
 }
